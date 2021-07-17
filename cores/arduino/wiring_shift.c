bf6fec1fec854b1c75160bb61c408a7e1fcdf7f8
Jesse Vincent <jesse@keyboard.io>
2021-07-16 20:53:02 -0700

    update wiring_shift to use ArduinoAPI-Core signatures

--- cores/arduino/wiring_shift.c
+++ cores/arduino/wiring_shift.c
@@ -22,7 +22,7 @@
 extern "C" {
 #endif
 
-uint32_t shiftIn(uint32_t ulDataPin, uint32_t ulClockPin, uint32_t ulBitOrder)
+uint8_t shiftIn(pin_size_t ulDataPin, pin_size_t ulClockPin, BitOrder ulBitOrder)
 {
     uint8_t value = 0 ;
     uint8_t i ;
@@ -42,7 +42,7 @@ uint32_t shiftIn(uint32_t ulDataPin, uint32_t ulClockPin, uint32_t ulBitOrder)
     return value ;
 }
 
-void shiftOut(uint32_t ulDataPin, uint32_t ulClockPin, uint32_t ulBitOrder, uint32_t ulVal)
+void shiftOut(pin_size_t ulDataPin, pin_size_t ulClockPin, BitOrder ulBitOrder, uint8_t ulVal)
 {
     uint8_t i ;
 
