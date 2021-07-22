ca1eaaed65ed39e03ae5dffdbb74a70cacb5ad25
Jesse Vincent <jesse@keyboard.io>
2021-07-22 00:17:02 -0700

    main.cpp had the wrong case for the Arduino include

--- cores/arduino/main.cpp
+++ cores/arduino/main.cpp
@@ -23,7 +23,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *****************************************************************************/
-#include "arduino.h"
+#include "Arduino.h"
 
 
 #ifdef __cplusplus
