e7ce18d609709b35912a24fc6d1f665928c2c223
Jesse Vincent <jesse@keyboard.io>
2021-07-23 15:51:23 -0700

    Fix case of Arduino include

--- cores/arduino/main.cpp
+++ cores/arduino/main.cpp
@@ -23,7 +23,7 @@
  * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  *****************************************************************************/
-#include "arduino.h"
+#include "Arduino.h"
 
 // Force init to be called *first*, i.e. before static object allocation.
 // Otherwise, statically allocated objects that need libmaple may fail.
