a68917a6c70a91eae9ae04624eb27f02fc11f475
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2021-08-05 20:24:50 +0200

    Add original copyright

--- libraries/Servo/examples/Knob/Knob.ino
+++ libraries/Servo/examples/Knob/Knob.ino
@@ -1,3 +1,11 @@
+/*
+ Controlling a servo position using a potentiometer (variable resistor)
+ by Michal Rinott <http://people.interaction-ivrea.it/m.rinott>
+ modified on 8 Nov 2013
+ by Scott Fitzgerald
+ http://www.arduino.cc/en/Tutorial/Knob
+*/
+
 #include <Servo.h>
 
 #define servo_pin 9
