f87a41f3123eb18404567b3bedbfb3ce6ab1aec8
unknown <maximilian.gerhardt@rub.de>
2021-10-02 20:20:59 +0200

    Work on interrupts

--- cores/arduino/gd32/gpio_interrupt.c
+++ cores/arduino/gd32/gpio_interrupt.c
@@ -45,44 +45,6 @@ extiConf_t gpio_exti_infor[EXTI_NUMS] = {
 #endif
 };
 
-#if defined(GD32F3x0)
-static uint8_t convert_gpio_port_to_exti_source(uint32_t gpio_periph) {
-    switch(gpio_periph) {
-        case GPIOA: return EXTI_SOURCE_GPIOA;
-        case GPIOB: return EXTI_SOURCE_GPIOB;
-        case GPIOC: return EXTI_SOURCE_GPIOC;
-        case GPIOD: return EXTI_SOURCE_GPIOD;
-        case GPIOF: return EXTI_SOURCE_GPIOF;
-        /* should never happen */
-        default: return EXTI_SOURCE_GPIOA;
-    }
-}
-
-static uint8_t convert_gpio_pin_to_exti_source(uint32_t gpio_pin) {
-    switch(gpio_pin) {
-        case GPIO_PIN_0: return EXTI_SOURCE_PIN0;
-        case GPIO_PIN_1: return EXTI_SOURCE_PIN1;
-        case GPIO_PIN_2: return EXTI_SOURCE_PIN2;
-        case GPIO_PIN_3: return EXTI_SOURCE_PIN3;
-        case GPIO_PIN_4: return EXTI_SOURCE_PIN4;
-        case GPIO_PIN_5: return EXTI_SOURCE_PIN5;
-        case GPIO_PIN_6: return EXTI_SOURCE_PIN6;
-        case GPIO_PIN_7: return EXTI_SOURCE_PIN7;
-        case GPIO_PIN_8: return EXTI_SOURCE_PIN8;
-        case GPIO_PIN_9: return EXTI_SOURCE_PIN9;
-        case GPIO_PIN_10: return EXTI_SOURCE_PIN10;
-        case GPIO_PIN_11: return EXTI_SOURCE_PIN11;
-        case GPIO_PIN_12: return EXTI_SOURCE_PIN12;
-        case GPIO_PIN_13: return EXTI_SOURCE_PIN13;
-        case GPIO_PIN_14: return EXTI_SOURCE_PIN14;
-        case GPIO_PIN_15: return EXTI_SOURCE_PIN15;
-        /* should never happen */
-        default: return EXTI_SOURCE_PIN0;
-    }
-}
-
-#endif
-
 void gpio_interrupt_enable(uint32_t portNum, uint32_t pinNum, void (*callback)(void), uint32_t mode)
 {
     exti_line_enum exti_line = BIT(pinNum);
@@ -103,11 +65,12 @@ void gpio_interrupt_enable(uint32_t portNum, uint32_t pinNum, void (*callback)(v
 
     #if defined(GD32F3x0)
     syscfg_exti_line_config(
-        convert_gpio_port_to_exti_source(gpio_port[portNum]), 
-        convert_gpio_pin_to_exti_source(pinNum));
+        (uint8_t) portNum), 
+        (uint8_t) pinNum));
+    #elif defined(GD32F30x)
+    gpio_exti_source_select(portNum, pinNum);
     #endif
 
-    gpio_exti_source_select(portNum, pinNum);
     exti_init(exti_line, exti_mode, trig_type);
     exti_interrupt_flag_clear(exti_line);
 
