b3cf94f9dadbff55a5f98146867a57f8777f07e2
unknown <maximilian.gerhardt@rub.de>
2021-10-19 22:42:54 +0200

    User auto-generated peripheral name header

--- variants/GD32F190C8_GENERIC/PeripheralNames.h
+++ variants/GD32F190C8_GENERIC/PeripheralNames.h
@@ -1,65 +1,73 @@
-/* mbed Microcontroller Library
- * Copyright (c) 2018 GigaDevice Semiconductor Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+/*
+    Copyright (c) 2021, CommunityGD32Cores
+
+    Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
 
 #ifndef PERIPHERALNAMES_H
 #define PERIPHERALNAMES_H
 
-#include "gd32f1x0.h"
+#include "gd32xxyy.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef enum {
-    ADC_0 = (int)ADC
+    ADC_0 = (int)ADC,
 } ADCName;
 
 typedef enum {
-    DAC_0 = (int)DAC,
+    DAC_0 = (int)DAC0,
+    DAC_1 = (int)DAC1,
 } DACName;
 
 typedef enum {
     UART_0 = (int)USART0,
-#ifdef USART1
-    UART_1 = (int)USART1
-#endif
+    UART_1 = (int)USART1,
 } UARTName;
 
 typedef enum {
     SPI_0 = (int)SPI0,
-#ifdef SPI1
-    SPI_1 = (int)SPI1
-#endif
+    SPI_1 = (int)SPI1,
+    SPI_2 = (int)SPI2,
 } SPIName;
 
 typedef enum {
     I2C_0 = (int)I2C0,
-#ifdef I2C1
-    I2C_1 = (int)I2C1
-#endif
+    I2C_1 = (int)I2C1,
+    I2C_2 = (int)I2C2,
 } I2CName;
 
 typedef enum {
     PWM_0 = (int)TIMER0,
     PWM_1 = (int)TIMER1,
     PWM_2 = (int)TIMER2,
-    PWM_5 = (int)TIMER5
-    /* Timer 14..16 are also there, are these usable for PWM? */
+    PWM_13 = (int)TIMER13,
+    PWM_14 = (int)TIMER14,
+    PWM_15 = (int)TIMER15,
+    PWM_16 = (int)TIMER16,
 } PWMName;
 
 
