3b08a64fa3be21ab938a40297e6264bc1d7c1c0e
unknown <maximilian.gerhardt@rub.de>
2021-10-25 23:19:36 +0200

    Add all F150 auto-generated variants

---
+++ variants/GD32F150G4_GENERIC/variant.h
@@ -0,0 +1,118 @@
+/*
+    Copyright (c) 2021, CommunityGD32Cores
+
+    Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+#ifndef _VARIANT_
+#define _VARIANT_
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+/* GPIO pins definitions */
+#define PF0 0
+#define PF1 1
+#define PA9 2
+#define PA10 3
+#define PA11 4
+#define PA12 5
+#define PA13 6
+#define PA14 7
+#define PA15 8
+#define PB3 9
+#define PB4 10
+#define PB5 11
+#define PB6 12
+#define PB7 13
+/* analog pins */
+#define PA0 14
+#define PA1 15
+#define PA2 16
+#define PA3 17
+#define PA4 18
+#define PA5 19
+#define PA6 20
+#define PA7 21
+#define PB0 22
+#define PB1 23
+
+/* digital pins and analog pins number definitions */
+#define DIGITAL_PINS_NUM            24
+#define ANALOG_PINS_NUM             10
+#define ANALOG_PINS_START           PA0
+#define ANALOG_PINS_LAST            PB1
+
+/* LED definitions */
+#define LED_BUILTIN                 PB2
+
+/* user keys definitions */
+#define KEY0                        PA0
+
+/* SPI definitions */
+#define PIN_SPI_SS                  PA15
+#define PIN_SPI_MOSI                PB5
+#define PIN_SPI_MISO                PB4
+#define PIN_SPI_SCK                 PB3
+
+/* I2C definitions */
+#define PIN_WIRE_SDA                PB7
+#define PIN_WIRE_SCL                PB6
+#define USE_I2C                     1
+
+/* TIMER or PWM definitions */
+#define TIMER_TONE                  TIMER5
+#define TIMER_SERVO                 TIMER6
+
+#define PWM0                        PA0
+#define PWM1                        PA1
+#define PWM2                        PA2
+#define PWM3                        PA3
+#define PWM4                        PA4
+
+/* USART definitions */
+#define SERIAL_HOWMANY              1
+#define USE_USART0_SERIAL           
+#define PIN_SERIAL_RX               PA3
+#define PIN_SERIAL_TX               PA2
+#define SERIAL0_RX                  PA3
+#define SERIAL0_TX                  PA2
+
+/* ADC definitions */
+#define ADC_RESOLUTION              10
+#define DAC_RESOLUTION              12
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#ifdef __cplusplus
+    /* Port which normally prints to the Arduino Serial Monitor */
+    #define SERIAL_PORT_MONITOR     Serial
+    /* Hardware serial port, physical RX & TX pins. */
+    #define SERIAL_PORT_HARDWARE    Serial1
+#endif
+
+#endif /* _VARIANT_ */
---
+++ variants/GD32F150K8_GENERIC/variant.cpp
@@ -0,0 +1,81 @@
+/*
+    Copyright (c) 2021, CommunityGD32Cores
+
+    Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    1. Redistributions of source code must retain the above copyright notice, this
+       list of conditions and the following disclaimer.
+    2. Redistributions in binary form must reproduce the above copyright notice,
+       this list of conditions and the following disclaimer in the documentation
+       and/or other materials provided with the distribution.
+    3. Neither the name of the copyright holder nor the names of its contributors
+       may be used to endorse or promote products derived from this software without
+       specific prior written permission.
+
+    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY
+OF SUCH DAMAGE.
+*/
+
+#include "pins_arduino.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* digital pins for pinmap list */
+const PinName digital_pins[] = {
+    PORTF_0,
+    PORTF_1,
+    PORTB_2,
+    PORTA_8,
+    PORTA_9,
+    PORTA_10,
+    PORTA_11,
+    PORTA_12,
+    PORTA_13,
+    PORTA_14,
+    PORTA_15,
+    PORTB_3,
+    PORTB_4,
+    PORTB_5,
+    PORTB_6,
+    PORTB_7,
+    PORTB_8,
+    PORTA_0,
+    PORTA_1,
+    PORTA_2,
+    PORTA_3,
+    PORTA_4,
+    PORTA_5,
+    PORTA_6,
+    PORTA_7,
+    PORTB_0,
+    PORTB_1
+};
+
+/* analog pins for pinmap list */
+const uint32_t analog_pins[] = {
+    PA0, //A0
+    PA1, //A1
+    PA2, //A2
+    PA3, //A3
+    PA4, //A4
+    PA5, //A5
+    PA6, //A6
+    PA7, //A7
+    PB0, //A8
+    PB1 //A9
+};
+
+#ifdef __cplusplus
+}
+#endif
